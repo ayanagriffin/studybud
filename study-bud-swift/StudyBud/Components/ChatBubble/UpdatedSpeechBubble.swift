9b3d88d2381f11b67ffa5fce0ecf50d1bf4717ec
Lauren Yu <laurenjiyeonyu@gmail.com>
2025-06-01 16:23:51 -0700

    updated chat bubble

--- study-bud-swift/StudyBud/Components/ChatBubble/UpdatedSpeechBubble.swift
+++ study-bud-swift/StudyBud/Components/ChatBubble/UpdatedSpeechBubble.swift
@@ -52,39 +52,3 @@ struct UpdatedSpeechBubble: Shape {
     }
 }
 
-
-struct ChatBubbleView: View {
-    var text: String
-    var tailPosition: CGFloat = 0.5 // Default is center
-
-    var body: some View {
-        Text(text)
-            .font(.speechText).fontWeight(.bold)
-            .foregroundColor(.black)
-            .multilineTextAlignment(.center)
-            .padding(EdgeInsets(top: 16, leading: 16, bottom: 48, trailing: 16))
-            .background(
-                UpdatedSpeechBubble(tailPosition: tailPosition)
-                    .fill(Color(white: 0.98))
-            )
-            .overlay(
-                UpdatedSpeechBubble(tailPosition: tailPosition)
-                    .stroke(Color.darkGray, lineWidth: 3)
-            )
-            .fixedSize(horizontal: false, vertical: true)
-            .padding()
-    }
-}
-
-
-#Preview("Tail in Center") {
-    ChatBubbleView(text: "Hey John! I’ve been itching to do some work.", tailPosition: 0.5)
-}
-
-#Preview("Tail on Left") {
-    ChatBubbleView(text: "This one’s aligned left.", tailPosition: 0.2)
-}
-
-#Preview("Tail on Right") {
-    ChatBubbleView(text: "Right-side bubble here.", tailPosition: 0.85)
-}
